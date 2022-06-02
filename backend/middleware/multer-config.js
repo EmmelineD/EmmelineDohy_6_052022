11bae0eb30b9e49ba2ab35d43527366d20923d70
EmmelineD <98889798+EmmelineD@users.noreply.github.com>
2022-06-02 12:08:02 +0200

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,14 +1,14 @@
 //Importation de multer
 const multer = require('multer');
 
-//Dictionnaire 
+//configuration des extensions possible
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
-//Configuration du chemin et nom de fichier
+//Configuration du chemin pour enregistrer le fichier et sous quel nom
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
@@ -16,9 +16,9 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, name + Date.now() + '.' + extension); // name = nom du fichier + extension
   }
 });
 
-//Exportation de multer
+//Exportation du middleware multer
 module.exports = multer({ storage }).single('image');
