9852c22778fc3c58e3d5c47be222fe5ad6cd59b1
EmmelineD <98889798+EmmelineD@users.noreply.github.com>
2022-06-02 12:24:02 +0200

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,17 +1,19 @@
-//Importation d'express
+//importation d'express
 const express = require('express');
 
-//Création du routeur
+//création des routes via express
 const router = express.Router();
 
-//Importation de password-validator
+//importation du model password
 const password = require('../middleware/password');
 
-//Importation du controller
+//importation du controller user
 const userCtrl = require('../controllers/user');
 
-//Création des routes 
+//création d'un user
 router.post('/signup', password, userCtrl.signup);
+//connexion d'un user déjà existant
 router.post('/login', userCtrl.login);
 
+//exporter les routes créées
 module.exports = router;
