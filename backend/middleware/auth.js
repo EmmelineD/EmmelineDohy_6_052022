e5c283e4b50160b06c0331e4eeb8e903c216606f
EmmelineD <98889798+EmmelineD@users.noreply.github.com>
2022-06-02 12:04:29 +0200

    modifications des commentaires

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,15 +1,15 @@
+//importation de jsonwebtoken pour créer le token d'accès
 const jwt = require('jsonwebtoken');
 
-//Importation de dotenv
+//importation de dotenv
 const dotenv = require('dotenv');
 dotenv.config();
 
 // vérification du token
-
 module.exports = (req, res, next) => {
   try { 
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.TOKEN);
+    const token = req.headers.authorization.split(' ')[1]; 
+    const decodedToken = jwt.verify(token, process.env.TOKEN); //utilisation de la variable TOKEN défini dans .env
     const userId = decodedToken.userId;
     req.auth = {userId: userId};  // (racourci JS = {userId})
     if (req.body.userId && req.body.userId !== userId) {
