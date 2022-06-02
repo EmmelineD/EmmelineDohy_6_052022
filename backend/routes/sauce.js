e35f10bc543bf5ce0a42f7875ae7dbdc090e4993
EmmelineD <98889798+EmmelineD@users.noreply.github.com>
2022-06-02 12:20:54 +0200

    Update sauce.js

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,16 +1,30 @@
+//Importation d'Express
 const express = require('express');
+
+//Création des routes via express
 const router = express.Router();
 
+//Importation du middleware d'authentification
 const auth = require('../middleware/auth');
+
+//Importation du middleware pour les images multer
 const multer = require('../middleware/multer-config');
 
+//Importation du controllers sauce
 const sauceCtrl = require('../controllers/sauce');
 
-router.get('/', auth, sauceCtrl.getAllSauces); // afficher tous les objets
-router.post('/', auth, multer, sauceCtrl.createSauce); // créer un objet (placer multer après auth sinon une image peut être enregistré sans l'authentification)
-router.get('/:id', auth, sauceCtrl.getOneSauce); // récupération d'un objet avec son id
-router.put('/:id', auth, multer, sauceCtrl.modifySauce); // modifier un objet
-router.delete('/:id', auth, sauceCtrl.deleteSauce); // supprimer un objet
-router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce); // like dislike
+// afficher tous les objets
+router.get('/', auth, sauceCtrl.getAllSauces); 
+// créer un objet (placer multer après auth sinon une image peut être enregistré sans l'authentification)
+router.post('/', auth, multer, sauceCtrl.createSauce);
+// récupération d'un objet avec son id
+router.get('/:id', auth, sauceCtrl.getOneSauce); 
+// modifier un objet
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+// supprimer un objet
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+// like dislike
+router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce);
 
+//exporter les routes créées
 module.exports = router;
